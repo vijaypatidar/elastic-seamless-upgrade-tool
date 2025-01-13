fb3408551518cb071f313cc693a6fc4319b7d431
Vijay <vijay.p@consultadd.com>
2025-01-13 20:30:38 +0530

    Configured ES client

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { ElacticClient } from './clients/elastic';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,28 @@ app.get('/health', (req: Request, res: Response) => {
   res.send({
     message: 'Server is healthy! 🚀',
   });
+});
+
+export interface ElacticClusterBaseRequest {
+  url: string;
+  apiKey?: string;
+  username?: string;
+  password?: string;
+  bearer?: string;
+}
+export interface ElacticClusterHealthRequest
+  extends ElacticClusterBaseRequest {}
+
+app.post('/api/elatic/health', async (req: Request, res: Response) => {
+  try {
+    const body: ElacticClusterHealthRequest = req.body;
+    const client = new ElacticClient(body);
+    const health = await client.getClusterhealth();
+    res.send(health);
+  } catch (err: any) {
+    console.log(err);
+    res.status(400).send({ message: err.message });
+  }
 });
 
 app.listen(PORT, () => {
