fcee28173569f10132ed510aa59bcfa8fddda934
Vijay <vijay.p@consultadd.com>
2025-01-22 20:35:04 +0530

    Added dotenv

--- backend/src/databases/db.ts
+++ backend/src/databases/db.ts
@@ -2,9 +2,11 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://admin:admin123@localhost:27017/',
-    );
+    const mongoUri = process.env.MONGO_URI;
+    if (!mongoUri) {
+      throw new Error('MongoDb connection URI required');
+    }
+    await mongoose.connect(mongoUri);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Database connection failed', err);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import elasticRouter from './routes/elasticRouter';
