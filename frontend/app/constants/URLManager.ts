bc1276dac1fb728bc2c0416bdec4d89f107c8586
srajan1202 <srajan.sohani@consultadd.com>
2025-03-15 01:16:31 +0530

    UPDATED: base url

--- frontend/app/constants/URLManager.ts
+++ frontend/app/constants/URLManager.ts
@@ -1,7 +1,8 @@
+const hostname = () => (typeof window !== "undefined" ? window.location.hostname : "");
 const URLManager = {
-    // HTTP_BASE_URL: `http://${window.location.hostname}:3000`,
+    HTTP_BASE_URL: `http://${hostname()}:3000`,
     // HTTP_BASE_URL: "http://localhost:3000",
-    HTTP_BASE_URL: "http://54.91.120.67:3000/",
+    // HTTP_BASE_URL: "http://54.91.120.67:3000/",
     SOCKET_BASE_URL: "",
 
     REFRESH_TOKEN_URL: "/refresh",
