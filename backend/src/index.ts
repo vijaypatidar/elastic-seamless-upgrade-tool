70f26223e65372e328233bb50a3ccbbee4a3f4d2
vijay patidar <vramotiya@gmail.com>
2025-04-02 13:42:55 +0530

    Integrated socket.io in express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,58 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv"
+import http from "http"
+import { Server, Socket } from "socket.io"
 
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import elasticRouter from './routes/elastic.router';
+dotenv.config()
 
-import logger from './logger/logger';
-import { connectDB } from './databases/db';
+import express, { Request, Response } from "express"
+import cors from "cors"
+import elasticRouter from "./routes/elastic.router"
 
-const app = express();
-const PORT = 3000;
+import logger from "./logger/logger"
+import { connectDB } from "./databases/db"
 
-app.use(cors());
-app.use(express.json());
+const app = express()
+const server = http.createServer(app)
 
-app.get('/health', (req: Request, res: Response) => {
-  res.send({
-    message: 'Server is healthy! 🚀',
-  });
-});
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/health", (req: Request, res: Response) => {
+	res.send({
+		message: "Server is healthy! 🚀",
+	})
+})
 
 export interface ElasticClusterBaseRequest {
-  url: string;
-  apiKey?: string;
-  username?: string;
-  password?: string;
-  bearer?: string;
-  ssl: object;
+	url: string
+	apiKey?: string
+	username?: string
+	password?: string
+	bearer?: string
+	ssl: object
 }
-export interface ElasticClusterHealthRequest
-  extends ElasticClusterBaseRequest {}
+export interface ElasticClusterHealthRequest extends ElasticClusterBaseRequest {}
 
 //routes
-app.use('/api/elastic/clusters', elasticRouter);
+app.use("/api/elastic/clusters", elasticRouter)
+
+const io = new Server(server)
+io.of("/notification").on("connection", (socket: Socket) => {
+	logger.debug("User connected to socker.io with socketId:", socket.id)
+
+	socket.on("message", (data) => {
+		logger.debug("Received message:", data)
+	})
+
+	socket.on("disconnect", () => {
+		logger.debug("User disconnected from socker.io with socketId:", socket.id)
+	})
+})
 
 connectDB().then(() => {
-  app.listen(PORT, async () => {
-    logger.info(`Server is running at http://localhost:${PORT}`);
-  });
-});
+	server.listen(PORT, async () => {
+		logger.info(`Server is running at http://localhost:${PORT}`)
+	})
+})
