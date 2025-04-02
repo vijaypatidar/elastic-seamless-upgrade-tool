25bcb915eb349d9ce0ab8640eca21474a0b50cae
vijay patidar <vramotiya@gmail.com>
2025-04-02 12:44:01 +0530

    Code format

--- backend/src/models/precheck.model.ts
+++ backend/src/models/precheck.model.ts
@@ -1,24 +1,20 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose"
 
 export interface IPrecheck {
-  name: string;
-  description: string;
+	name: string
+	description: string
 }
 
-export interface IPrecheckDocument extends IPrecheck, Document { }
+export interface IPrecheckDocument extends IPrecheck, Document {}
 
-const PrecheckSchema: Schema<IPrecheckDocument> =
-  new Schema<IPrecheckDocument>(
-    {
-      name: { type: String, required: true, unique: true },
-      description: { type: String, required: true, unique: true }
-    },
-    { timestamps: true },
-  );
+const PrecheckSchema: Schema<IPrecheckDocument> = new Schema<IPrecheckDocument>(
+	{
+		name: { type: String, required: true, unique: true },
+		description: { type: String, required: true, unique: true },
+	},
+	{ timestamps: true }
+)
 
-const Precheck = mongoose.model<IPrecheckDocument>(
-  'Precheck',
-  PrecheckSchema,
-);
+const Precheck = mongoose.model<IPrecheckDocument>("Precheck", PrecheckSchema)
 
-export default Precheck;
+export default Precheck
