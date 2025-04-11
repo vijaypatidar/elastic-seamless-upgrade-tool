49ab23ae84b4691939675037eaedb9f501e547c5
vijay patidar <vramotiya@gmail.com>
2025-04-11 14:00:36 +0530

    Added new event type for precheck progress change

--- backend/src/controllers/settings-controller.ts
+++ backend/src/controllers/settings-controller.ts
@@ -1,16 +1,12 @@
 import { Request, Response } from "express";
 import logger from "../logger/logger";
-import { ISettings, SettingsModel } from "../models/settings.model";
+import { ISettings } from "../models/settings.model";
+import { settingService } from "../services/settings.service";
 
 export const updateSettings = async (req: Request, res: Response) => {
 	try {
 		const { notificationWebhookUrl }: ISettings = req.body;
-		const id = "settings";
-		await SettingsModel.findOneAndUpdate(
-			{ settingsId: id },
-			{ $set: { notificationWebhookUrl } },
-			{ new: true, upsert: true, runValidators: true }
-		);
+		settingService.updateSettings({ notificationWebhookUrl });
 		res.send({ message: "Settings updated" });
 	} catch (err: any) {
 		logger.info(err);
@@ -20,7 +16,7 @@ export const updateSettings = async (req: Request, res: Response) => {
 
 export const getSettings = async (req: Request, res: Response) => {
 	try {
-		const settings = await SettingsModel.findOne({ settingsId: "settings" });
+		const settings = await settingService.getSettings();
 		res.send({
 			notificationWebhookUrl: settings?.notificationWebhookUrl,
 		});
