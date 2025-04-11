91e3ac74f112c7d2312f59823f3d044e62ace692
vijay patidar <vramotiya@gmail.com>
2025-04-12 01:05:25 +0530

    re-run prechecks by nodeId

--- backend/src/controllers/prechecks-controller.ts
+++ backend/src/controllers/prechecks-controller.ts
@@ -1,32 +1,26 @@
 import { Request, Response } from "express";
 import { getAllElasticNodes, getElasticNodeById } from "../services/elastic-node.service.";
 import { getLatestRunsByPrecheck, runPrecheck } from "../services/precheck-runs.service";
-import { ELASTIC_PRECHECK_CONFIG, getPrecheckById } from "../config/precheck-config";
+import { getPrecheckById } from "../config/precheck-config";
 import { PrecheckStatus } from "../enums";
 import { INodePrecheckRun } from "../models/node-precheck-runs.model";
 
 export const runAllPrecheksHandler = async (req: Request, res: Response) => {
 	const { clusterId } = req.params;
 	const nodes = await getAllElasticNodes(clusterId);
-	const precheckIds: string[] = ELASTIC_PRECHECK_CONFIG.individuals.map((precheck) => precheck.id);
-	const runId = await runPrecheck(precheckIds, nodes, clusterId);
+	const runId = await runPrecheck(nodes, clusterId);
 	res.send({ message: "Prechecks started", runId });
 };
 
 export const runPrechekByNodeIdHandler = async (req: Request, res: Response) => {
-	const { clusterId, precheckId, nodeId } = req.params;
+	const { clusterId, nodeId } = req.params;
 	const node = await getElasticNodeById(nodeId);
 	if (!node) {
 		res.status(404).send({ message: "Node not found" });
 		return;
 	}
 
-	const selectedPrecheck = ELASTIC_PRECHECK_CONFIG.individuals.filter((precheck) => precheckId === precheck.id);
-	if (!selectedPrecheck) {
-		res.status(404).send({ message: "Precheck not found" });
-		return;
-	}
-	const runId = await runPrecheck([precheckId], [node], clusterId);
+	const runId = await runPrecheck([node], clusterId);
 
 	res.send({ message: "Prechecks started", runId });
 };
