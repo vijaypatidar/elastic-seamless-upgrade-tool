752b6003b1ee2b2e9215cdae5306e0ff527c0457
vijay patidar <vramotiya@gmail.com>
2025-04-16 12:25:48 +0530

    Updated backend code to run new precheck and code refator

--- backend/src/controllers/prechecks-controller.ts
+++ backend/src/controllers/prechecks-controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { getAllElasticNodes, getElasticNodeById } from "../services/elastic-node.service.";
 import { getLatestRunsByPrecheck, runPrecheck } from "../services/precheck-runs.service";
-import { ELASTIC_PRECHECK_CONFIG } from "../config/precheck-config";
+import { PRECHECK_CONFIG } from "../config/precheck-config";
 import { PrecheckStatus } from "../enums";
 
 export const runAllPrecheksHandler = async (req: Request, res: Response) => {
 	const { clusterId } = req.params;
 	const nodes = await getAllElasticNodes(clusterId);
-	const precheckIds: string[] = ELASTIC_PRECHECK_CONFIG.individuals.map((precheck) => precheck.id);
+	const precheckIds: string[] = PRECHECK_CONFIG.map((precheck) => precheck.id);
 	const runId = await runPrecheck(precheckIds, nodes, clusterId);
 	res.send({ message: "Prechecks started", runId });
 };
@@ -20,7 +20,7 @@ export const runPrechekByNodeIdHandler = async (req: Request, res: Response) => 
 		return;
 	}
 
-	const selectedPrecheck = ELASTIC_PRECHECK_CONFIG.individuals.filter((precheck) => precheckId === precheck.id);
+	const selectedPrecheck = PRECHECK_CONFIG.filter((precheck) => precheckId === precheck.id);
 	if (!selectedPrecheck) {
 		res.status(404).send({ message: "Precheck not found" });
 		return;
@@ -44,7 +44,7 @@ export const getPrecheckRunByClusterIdHandler = async (req: Request, res: Respon
 		acc[run.precheckId].push(run);
 		return acc;
 	}, {});
-	const response = ELASTIC_PRECHECK_CONFIG.individuals.map((precheck) => {
+	const response = PRECHECK_CONFIG.map((precheck) => {
 		const precheckRuns = groupedPrecheckRuns[precheck.id];
 		const status = (() => {
 			let hasCompleted = false;
