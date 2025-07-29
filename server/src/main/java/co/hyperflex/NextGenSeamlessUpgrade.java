2c0e4c9c1136140efd0abf43026f05c40d12de87
vijay patidar <vramotiya@gmail.com>
2025-07-29 16:28:34 +0530

    Use an Async task for prechecks task execution

--- server/src/main/java/co/hyperflex/NextGenSeamlessUpgrade.java
+++ server/src/main/java/co/hyperflex/NextGenSeamlessUpgrade.java
@@ -3,8 +3,10 @@ package co.hyperflex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NextGenSeamlessUpgrade {
   public static void main(String[] args) {
     SpringApplication.run(NextGenSeamlessUpgrade.class, args);
