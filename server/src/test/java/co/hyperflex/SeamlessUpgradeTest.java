4dde61e415b0a682c1ae6fa7944281db529c9b02
vijaypatidar <vramotiya@gmail.com>
2025-08-05 21:31:25 +0530

    Added context load test

---
+++ server/src/test/java/co/hyperflex/SeamlessUpgradeTest.java
@@ -0,0 +1,14 @@
+package co.hyperflex;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SeamlessUpgradeTest {
+
+  @Test
+  public void loadContext() {
+
+  }
+}
