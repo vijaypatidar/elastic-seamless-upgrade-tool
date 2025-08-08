819988cce23b4a0631740a53c1e1862e21d38896
vijaypatidar <vramotiya@gmail.com>
2025-08-08 12:18:54 +0530

    Removed backend(nodejs) to clean up repo

--- backend/src/routes/webhook.router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { handleAnsibleWebhook } from "../controllers/ansible-webhooks.controller";
-const router = Router();
-
-router.post("/clusters/:clusterId/update-status", handleAnsibleWebhook);
-
-export default router;
