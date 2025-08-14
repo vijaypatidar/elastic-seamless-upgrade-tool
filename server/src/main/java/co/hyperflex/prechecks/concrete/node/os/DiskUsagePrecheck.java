2e405df6603c8318617494643c747d2fe2ea3f8a
vijay patidar <vramotiya@gmail.com>
2025-08-14 17:20:43 +0530

    Set the threshold limit to 85% for disk check

--- server/src/main/java/co/hyperflex/prechecks/concrete/node/os/DiskUsagePrecheck.java
+++ server/src/main/java/co/hyperflex/prechecks/concrete/node/os/DiskUsagePrecheck.java
@@ -21,8 +21,13 @@ public class DiskUsagePrecheck extends BaseNodePrecheck {
       try (SshCommandExecutor executor = context.getSshExecutor()) {
         CommandResult result = executor.execute("df -h / | tail -n 1 | awk '{ print $5 }' | tr -d %");
         if (result.isSuccess()) {
+          int usage = Integer.parseInt(result.stdout());
           context.getLogger().info("Disk Utilization check completed");
-          context.getLogger().info("Disk Utilized: {}%", result.stdout());
+          context.getLogger().info("Disk Utilized: {}%", usage);
+          if (usage >= 85) {
+            context.getLogger().error("Disk Utilization exceeded the threshold of 85%");
+            throw new RuntimeException();
+          }
         } else {
           throw new RuntimeException(result.stderr());
         }
