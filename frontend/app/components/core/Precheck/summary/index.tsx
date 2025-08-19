0e560ff95a339babddccf19fc7bbe8a2e4d75ae6
vijay patidar <vramotiya@gmail.com>
2025-08-19 14:28:49 +0530

    Fixed precheck summary pill padding

--- frontend/app/components/core/Precheck/summary/index.tsx
+++ frontend/app/components/core/Precheck/summary/index.tsx
@@ -1,6 +1,7 @@
 import { Alarm, ArrowCircleRight2 } from "iconsax-react"
 import { FiAlertTriangle } from "react-icons/fi"
 import { usePrecheckSummary } from "~/lib/hooks/usePrecheckSummary"
+import { Box } from "@mui/material"
 
 type PrecheckType = "warning" | "critical" | "skipped"
 
@@ -32,24 +33,26 @@ function PrecheckSummaryItem({ type, count }: { type: PrecheckType; count: numbe
 	const { bg, color, label, Icon } = config[type]
 
 	return (
-		<div className={`px-3 py-0 ${bg} rounded-3xl inline-flex justify-center items-center gap-1 overflow-hidden`}>
-			<div className="flex justify-start items-center gap-1">
+		<Box
+			className={`px-[7px] py-[5px] ${bg} rounded-3xl inline-flex justify-center items-center gap-1 overflow-hidden`}
+		>
+			<Box className="flex justify-start items-center gap-1">
 				<Icon size="14px" color={color} />
-				<div className="justify-start text-xs font-medium font-['Inter']" style={{ color }}>
+				<Box className="justify-start text-[12px] font-medium font-['Inter']" style={{ color }}>
 					{label} {count}
-				</div>
-			</div>
-		</div>
+				</Box>
+			</Box>
+		</Box>
 	)
 }
 
 export function PrecheckSummary() {
 	const precheckSummary = usePrecheckSummary()
 	return (
-		<>
+		<Box className="flex items-center justify-center gap-[6px]">
 			<PrecheckSummaryItem count={precheckSummary.warning} type="warning" />
 			<PrecheckSummaryItem count={precheckSummary.critical} type="critical" />
 			<PrecheckSummaryItem count={precheckSummary.skipped} type="skipped" />
-		</>
+		</Box>
 	)
 }
