6146a62a5110e9566037cce2be828e585aacb92a
vijay patidar <vramotiya@gmail.com>
2025-08-26 13:52:34 +0530

    Fix plugin not listed in precheck

--- server/src/main/java/co/hyperflex/prechecks/concrete/node/elastic/CustomPluginsListPrecheck.java
+++ server/src/main/java/co/hyperflex/prechecks/concrete/node/elastic/CustomPluginsListPrecheck.java
@@ -6,33 +6,12 @@ import co.hyperflex.clients.elastic.dto.nodes.PluginStats;
 import co.hyperflex.entities.precheck.PrecheckSeverity;
 import co.hyperflex.prechecks.contexts.NodeContext;
 import co.hyperflex.prechecks.core.BaseElasticNodePrecheck;
-import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomPluginsListPrecheck extends BaseElasticNodePrecheck {
-
-  private static final Set<String> BUNDLED_PLUGINS = Set.of(
-      "x-pack-core",
-      "x-pack-security",
-      "x-pack-ml",
-      "x-pack-monitoring",
-      "x-pack-apm",
-      "x-pack-logstash",
-      "x-pack-deprecation",
-      "x-pack-ilm",
-      "x-pack-sql",
-      "x-pack-rollup",
-      "x-pack-stack",
-      "x-pack-ccr",
-      "x-pack-analytics",
-      "repository-gcs",
-      "repository-azure",
-      "repository-s3"
-  );
 
   @Override
   public String getName() {
@@ -64,23 +43,10 @@ public class CustomPluginsListPrecheck extends BaseElasticNodePrecheck {
       return;
     }
 
-    List<String> installedPlugins = nodeInfo.getPlugins().stream()
+    logger.info("Node [{}] has manually installed plugins:", nodeInfo.getName());
+    nodeInfo.getPlugins().stream()
         .map(PluginStats::getName)
         .filter(Objects::nonNull)
-        .toList();
-
-    List<String> customPlugins = installedPlugins.stream()
-        .filter(name -> !BUNDLED_PLUGINS.contains(name))
-        .toList();
-
-    if (customPlugins.isEmpty()) {
-      logger.info("Node [{}] has no manually installed plugins.", nodeInfo.getName());
-    } else {
-      logger.info(
-          "Node [{}] has manually installed plugins: {}",
-          nodeInfo.getName(),
-          String.join(", ", customPlugins)
-      );
-    }
+        .forEach(plugin-> logger.info("* {}", plugin));
   }
 }
